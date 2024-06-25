bc097dab10f129382e8bbea6582a16c2da6cf917
keimha <keimham@yahoo.com>
2024-06-24 21:15:38 -0500

    second commit

---
+++ loopsalgo.js
@@ -0,0 +1,12 @@
+// print odd 1-20 algorithm
+for(let i = 1; i <= 20; i++) {
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+}
+// sum and print 1-5 algorithm
+let sum = 0;
+for(let i = 1; i <= 5; i++) {
+    sum += i;
+    console.log('Num: ' + i + ', Sum: ' + sum);
+}
